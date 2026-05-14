2b91b7193e95ad6f5ab37345fb4199d994352e3c
hosik-2 <zxvbcd123@gmail.com>
2026-05-15 01:29:47 +0900

    회원가입 테스트 수정

--- src/test/java/com/hoseok/firstboard/web/member/MemberControllerTest.java
+++ src/test/java/com/hoseok/firstboard/web/member/MemberControllerTest.java
@@ -3,6 +3,7 @@ package com.hoseok.firstboard.web.member;
 import com.hoseok.firstboard.domain.member.Member;
 import com.hoseok.firstboard.domain.member.MemberService;
 import com.hoseok.firstboard.domain.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.jupiter.api.Assertions.*;
@@ -88,11 +92,15 @@ class MemberControllerTest {
                         .param("loginId", "loginId")
                         .param("password", "correctpw")
                         .param("email", "correct@email.com")
-                        .param("phoneNumber", "00000000"))
+                        .param("phoneNumber", "00000000000"))
                 .andExpect(status().is3xxRedirection())
-                .andExpect(model().attribute("loginId", "loginId"))
                 .andExpect(redirectedUrl("/"))
                 .andDo(print());
+
+        assertThat(memberService.findLoginId("name", "correct@email.com", "00000000000")).isNotEmpty();
+        assertThat(memberService.findLoginId("name", "correct@email.com", "00000000000"))
+                .isPresent()
+                .hasValue("loginId"); // Optional을 처리하는 방법
 
     }
 
